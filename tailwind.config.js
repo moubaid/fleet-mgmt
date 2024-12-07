e76f953166bf78f268e781aef1ddfc54544ae16a
Mohammed Oubaidullah <m.oubaid@gmail.com>
2024-12-07 23:05:57 +0300

    fix for broder error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './index.html',
+    './app/**/*.{css,js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{css,js,ts,jsx,tsx,mdx}',
+    './components/**/*.{css,js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/**/*.{css,js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
